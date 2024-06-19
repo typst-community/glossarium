57301c3f2f3ed685e546cef1c47ef5d1c41a9682
slashformotion <slashformotion@protonmail.com>
2024-06-19 19:38:11 +0200

    add tbump to flake.nix

--- flake.nix
+++ flake.nix
@@ -3,25 +3,30 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    master.url = "github:nixos/nixpkgs/master";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = {
     self,
     nixpkgs,
+    master,
     flake-utils,
   }:
     flake-utils.lib.eachDefaultSystem
     (
       system: let
         pkgs = nixpkgs.legacyPackages.${system};
+        masterPkgs = master.legacyPackages.${system};
       in {
         devShell = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            typst
-            just
-            zathura
-            typstyle
+          buildInputs = [
+            pkgs.typst
+            pkgs.just
+            pkgs.zathura
+            pkgs.typstyle
+
+            masterPkgs.tbump
           ];
         };
       }
