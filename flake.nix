c89c2426455b16011e2d3177d52883294d3ac0d8
Théophile Roos <45801817+slashformotion@users.noreply.github.com>
2024-11-05 17:18:19 +0100

    flake: input master is no longer needed

--- flake.nix
+++ flake.nix
@@ -3,21 +3,18 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    master.url = "github:nixos/nixpkgs/master";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = {
     self,
     nixpkgs,
-    master,
     flake-utils,
   }:
     flake-utils.lib.eachDefaultSystem
     (
       system: let
         pkgs = nixpkgs.legacyPackages.${system};
-        masterPkgs = master.legacyPackages.${system};
       in {
         devShell = pkgs.mkShell {
           buildInputs = [
@@ -25,8 +22,7 @@
             pkgs.just
             pkgs.zathura
             pkgs.typstyle
-
-            masterPkgs.tbump
+            pkgs.tbump
           ];
         };
       }
